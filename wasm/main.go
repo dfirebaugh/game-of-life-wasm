246fbed38687dcf4b85b37d087d3c914d6fecfd0
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-02-17 15:11:56 -0500

    - now rendering to the DOM

--- wasm/main.go
+++ wasm/main.go
@@ -12,10 +12,16 @@ import (
 
 // SIZE size of the grid
 //- i.e. the width and height of the grid
-const SIZE = 10
+const SIZE = 50
+
+// CELLSIZE size of cells in pixels
+const CELLSIZE = 10
 
 // TICKSPEED is how fast the generations update
 const TICKSPEED = 1
+
+// CELLBORDERSIZE is how big the border for each cell should be
+const CELLBORDERSIZE = 1
 
 type cell struct {
 	alive bool
@@ -26,46 +32,71 @@ type coords struct {
 	y int
 }
 
+// DOMNodes - contains nodes on the DOM
+type DOMNodes struct {
+	Grid         js.Value
+	PlayBtn      js.Value
+	ResetBtn     js.Value
+	GenBtn       js.Value
+	msgContainer js.Value
+	message      js.Value
+	genLabel     js.Value
+	generation   js.Value
+	btnContainer js.Value
+	ClearBtn     js.Value
+}
+
 // Game holds the state of our cells
 type Game struct {
-	b          bool
 	isPaused   bool
 	generation int
 	message    string
 	printlog   bool
 	speed      time.Duration
 	cells      [SIZE][SIZE]cell
+	dom        DOMNodes
 }
 
-// initBoard - setup game
+// initGame - setup game
 func (g *Game) initGame() {
-	js.Global().Set("BOARD_SIZE", SIZE)
-	g.printlog = true
-	g.isPaused = true
+	js.Global().Set("GRID_SIZE", SIZE)
+	g.printlog = false
 	g.speed = TICKSPEED
 
 	g.reset()
 
-	g.registerCallbacks()
+	g.startPolling()
 }
 
 func (g *Game) reset() {
+	g.isPaused = true
 	g.generation = 0
+	g.message = ""
 	g.updateCells(randomAlive)
+	g.initDOMNodes()
 	g.logger("reset!")
+	g.render()
+}
 
-	g.boardToJS()
-	chunk(g.toBinaryStr(true), SIZE)
-	g.print()
-	g.printToDOM()
+func (g *Game) clearGrid() {
+
+	g.updateCells(func(c cell, xy coords) cell {
+		c.alive = false
+		return c
+	})
+
+	g.isPaused = true
+	g.generation = 0
+	g.message = "cleared"
+	g.logger("cleared")
+	g.render()
 }
 
 // registerCallbacks defines callbacks that are used within JS code
-// A loop is created to prevent the go code from exiting
 func (g *Game) registerCallbacks() {
 	var runCb, generateCb js.Func
 	runCb = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		g.boardToJS()
+		g.gridToJS()
 		g.togglePause()
 		// runCb.Release() // release the function if the button will not be clicked again
 		return nil
@@ -83,12 +114,16 @@ func (g *Game) registerCallbacks() {
 		return nil
 	})
 
-	// wait := make(chan struct{}, 0)
-	js.Global().Get("document").Call("getElementById", "runBtn").Call("addEventListener", "click", runCb)
-	js.Global().Get("document").Call("getElementById", "generateBtn").Call("addEventListener", "click", generateCb)
-	js.Global().Get("document").Call("getElementById", "resetBtn").Call("addEventListener", "click", resetCb)
-	g.startPolling()
-	// <-wait
+	clearCb := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		g.clearGrid()
+
+		return nil
+	})
+
+	g.dom.PlayBtn.Call("addEventListener", "click", runCb)
+	g.dom.GenBtn.Call("addEventListener", "click", generateCb)
+	g.dom.ResetBtn.Call("addEventListener", "click", resetCb)
+	g.dom.ClearBtn.Call("addEventListener", "click", clearCb)
 }
 
 // chunk splits a string into string slice of a given length
@@ -112,7 +147,7 @@ func (g *Game) logger(a ...interface{}) {
 // toBinaryStr - flatten the cells and convert to a string of binary numbers
 // because apparently sharing values to JS of certain types is difficult
 //
-// If the pretty option is true, the return will include line endings based on the boards SIZE
+// If the pretty option is true, the return will include line endings based on the grids SIZE
 func (g *Game) toBinaryStr(pretty bool) string {
 
 	var acc []string
@@ -135,8 +170,9 @@ func (g *Game) toBinaryStr(pretty bool) string {
 	return binaryStr
 }
 
-func (g *Game) boardToJS() {
-	js.Global().Set("currentBoard", g.toBinaryStr(false))
+// gridToJS sends the binary string of the current grid to a global variable in js
+func (g *Game) gridToJS() {
+	js.Global().Set("currentGrid", g.toBinaryStr(false))
 }
 
 func (g *Game) checkRules(c cell, xy coords) cell {
@@ -191,30 +227,101 @@ func d2(seed int64) bool {
 	return r1.Intn(2) == 1
 }
 
-func (g *Game) print() {
+func (g *Game) render() {
 	g.logger(g.toBinaryStr(true))
+	g.renderMessage()
+	g.updateDOMGrid()
 }
 
-func (g *Game) printToDOM() {
-	grid := js.Global().Get("document").Call("getElementById", "gridContainer")
+func (g *Game) createButtons() {
+	document := js.Global().Get("document")
+	g.dom.btnContainer = document.Call("getElementById", "btnContainer")
+	g.dom.btnContainer.Set("innerHTML", "") // clear container
 
-	grid.Set("innerHTML", g.toBinaryStr(true))
+	g.dom.PlayBtn = document.Call("createElement", "button")
+	g.dom.PlayBtn.Set("id", "runBtn")
+	g.dom.btnContainer.Call("appendChild", g.dom.PlayBtn)
+
+	g.dom.ResetBtn = document.Call("createElement", "button")
+	g.dom.ResetBtn.Set("innerHTML", "Reset")
+	g.dom.ResetBtn.Set("id", "resetBtn")
+	g.dom.btnContainer.Call("appendChild", g.dom.ResetBtn)
+
+	g.dom.GenBtn = document.Call("createElement", "button")
+	g.dom.GenBtn.Set("innerHTML", "Generate")
+	g.dom.GenBtn.Set("id", "generateBtn")
+	g.dom.btnContainer.Call("appendChild", g.dom.GenBtn)
+
+	g.dom.ClearBtn = document.Call("createElement", "button")
+	g.dom.ClearBtn.Set("innerHTML", "Clear")
+	g.dom.ClearBtn.Set("id", "ClearBtn")
+	g.dom.btnContainer.Call("appendChild", g.dom.ClearBtn)
+
+	g.registerCallbacks()
 }
 
-func (g *Game) printMessage() {
+func (g *Game) initDOMNodes() {
 	document := js.Global().Get("document")
 
-	genLabel := document.Call("getElementById", "generation-label")
-	genLabel.Set("innerHTML", "Generation: ")
+	g.dom.Grid = document.Call("getElementById", "gridContainer")
 
-	gen := document.Call("getElementById", "generation")
-	gen.Set("innerHTML", g.generation)
+	g.createButtons()
 
-	if len(g.message) > 0 {
-		g.logger("message: ", g.message)
-		msg := document.Call("getElementById", "message")
-		msg.Set("innerHTML", g.message)
+	/*
+		width of the grid is determined by SIZE (row width) * the size of a Cell with an
+		accomidation for border size of each cell (i.e. the left border and the right border)
+	*/
+	g.dom.Grid.Set("style", fmt.Sprintf("width: %dpx", SIZE*CELLSIZE+((CELLBORDERSIZE*SIZE)*2)))
+
+	g.dom.Grid.Set("innerHTML", "") // reset childNodes
+
+	for x, row := range g.cells {
+		rowNode := document.Call("createElement", "row")
+		for y, cell := range row {
+			cellNode := document.Call("createElement", "cell")
+			cellNode.Call("setAttribute", "alive", cell.alive)
+			cellNode.Set("id", fmt.Sprintf("cell-%d-%d", x, y))
+			// TODO: move away from pixels and use something more dynamic
+			cellNode.Set("style", fmt.Sprintf("width: %dpx; height: %dpx; border: solid %dpx", CELLSIZE, CELLSIZE, CELLBORDERSIZE))
+			rowNode.Call("appendChild", cellNode)
+		}
+		g.dom.Grid.Call("appendChild", rowNode)
 	}
+
+	g.dom.msgContainer = document.Call("getElementById", "messageContainer")
+	g.dom.msgContainer.Set("innerHTML", "") // clear out message container
+
+	g.dom.genLabel = document.Call("createElement", "generation-label")
+	g.dom.genLabel.Set("innerHTML", "Generation: 0")
+
+	g.dom.generation = document.Call("createElement", "generation")
+	g.dom.generation.Set("innerHTML", g.generation)
+
+	g.logger("message: ", g.message)
+	g.dom.message = document.Call("createElement", "message")
+	g.dom.message.Set("innerHTML", g.message)
+
+	g.dom.msgContainer.Call("appendChild", g.dom.genLabel)
+	g.dom.msgContainer.Call("appendChild", g.dom.generation)
+	g.dom.msgContainer.Call("appendChild", g.dom.message)
+}
+
+func (g *Game) updateDOMGrid() {
+	document := js.Global().Get("document")
+
+	// TODO: this would be better if we only change what needs to be changed
+	for x, row := range g.cells {
+		for y, cell := range row {
+			domCell := document.Call("getElementById", fmt.Sprintf("cell-%d-%d", x, y))
+			domCell.Call("setAttribute", "alive", cell.alive)
+		}
+	}
+}
+
+func (g *Game) renderMessage() {
+	g.dom.genLabel.Set("innerHTML", "Generation: ")
+	g.dom.generation.Set("innerHTML", g.generation)
+	g.dom.message.Set("innerHTML", g.message)
 }
 
 func (g *Game) generate() {
@@ -224,23 +331,22 @@ func (g *Game) generate() {
 	if reflect.DeepEqual(tmpCells, g.cells) {
 		g.message = "graph did not change - pausing..."
 		g.isPaused = false
-		g.printMessage()
+		g.render()
 		return
 	}
 	g.generation++
-	g.printMessage()
-	g.print()
-	g.printToDOM()
+	g.render()
 }
 
+// startPolling creates an infinite loop which is important because it prevents the go code from exiting
 func (g *Game) startPolling() {
-	btn := js.Global().Get("document").Call("getElementById", "runBtn")
 	for {
+		// TODO: only update this when it changes
 		if !g.isPaused {
 			go g.generate()
-			btn.Set("innerHTML", "Pause")
+			g.dom.PlayBtn.Set("innerHTML", "Pause")
 		} else {
-			btn.Set("innerHTML", "Play")
+			g.dom.PlayBtn.Set("innerHTML", "Play")
 		}
 		time.Sleep(g.speed * time.Second)
 	}
